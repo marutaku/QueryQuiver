895f225587adb3d27b26ecdc2bbb423613f3ca11
Takumi Maruyama <satouredstone@gmail.com>
2024-01-27 14:10:35 +0900

    Add logging and error handling in ChromeHistory and Downloader classes

--- query_quiver/downloader.py
+++ query_quiver/downloader.py
@@ -14,33 +14,40 @@ class Downloader(object):
         self.logger = create_logger(__name__)
         self.client = client
         self.origin_locks: dict = {}
+        self.results: list[str] = []
 
     def extract_information_from_webpages(self, urls: list[str]) -> list[WebPageInfo]:
         """Extract information from webpage"""
         html_list = self.download_webpages(urls)
         return [self.parse_webpage_info(html) for html in html_list]
 
-    def download_webpages(self, urls: list[str]) -> list[str]:
-        """Download webpages from URLs"""
-        results = []
-
-        def download(url: str):
-            """Download webpage from URL"""
-            origin = parse_url(url).host
-            with self.origin_locks.setdefault(origin, threading.Lock()):
+    def _download(self, url: str):
+        """Download webpage from URL"""
+        origin = parse_url(url).host
+        with self.origin_locks.setdefault(origin, threading.Lock()):
+            try:
+                self.logger.debug(f"Downloading {url}")
                 response = self.client.get(url)
                 time.sleep(1)
-                results.append(response.text)
+                self.results.append(response.text)
+            except Exception as e:
+                self.logger.warning(f"Failed to download {url}: {e}")
 
+    def download_webpages(self, urls: list[str]) -> list[str]:
+        """Download webpages from URLs"""
+        # reset results
+        self.results = []
         threads = []
+        self.logger.debug(f"Downloading {len(urls)} webpages")
         for url in urls:
-            thread = threading.Thread(target=download, args=(url,))
+            thread = threading.Thread(target=self._download, args=(url,))
             threads.append(thread)
             thread.start()
 
         for thread in threads:
             thread.join()
-        return results
+        self.logger.debug(f"Downloaded {len(self.results)} webpages")
+        return self.results
 
     def parse_webpage_info(self, html: str) -> WebPageInfo:
         """Parse webpage info from HTML
